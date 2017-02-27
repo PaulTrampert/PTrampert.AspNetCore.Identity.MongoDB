31741e55e7bb88b425e7d7ad66c31f29a917bc20
Paul Trampert <paul.trampert@gmail.com>
2017-02-26 19:05:38 -0500

    Getting the project all set up.

---
+++ Jenkinsfile
@@ -0,0 +1,8 @@
+nugetPipeline {
+	project = 'PTrampert.AspNetCore.Identity.MongoDB'
+	deployNupkg = true
+	nugetCredentialsId = 'nugetApiKey'
+	deploySymbols = true
+	symbolsCredentialsId = 'nugetApiKey'
+	symbolServer = 'https://nuget.smbsrc.net/'
+}
---
+++ PTrampert.AspNetCore.Identity.MongoDB.Test/DummyTest.cs
@@ -0,0 +1,16 @@
+﻿using Xunit;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PTrampert.AspNetCore.Identity.MongoDB.Test
+{
+    public class DummyTest
+    {
+        [Fact]
+        public void DoesNothing()
+        {
+            Assert.True(true);
+        }
+    }
+}
